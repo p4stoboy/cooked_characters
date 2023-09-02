f18fbf8600cbcf8c1271a114243e7eb3560b60ff
p4stoboy <b1gh@protronmail.com>
2023-09-02 16:06:38 +1000

    replaceAll

--- src/commands/add_char_command.ts
+++ src/commands/add_char_command.ts
@@ -5,7 +5,7 @@ import {CharacterProps} from "../types/CharacterProps";
 
 export const add_char_command = async (client: Client, guild_id: string, char: CharacterProps) => {
     const command = {
-        name: char.name.replace(" ", "_").toLowerCase(),
+        name: char.name.replaceAll(" ", "_").toLowerCase(),
         description: `Say something to ${char.name}`,
         options: [
             {
