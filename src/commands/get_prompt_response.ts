43c376d68a9c291272f6ec7dd9a04d55e8a3defc
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2023-09-02 17:29:33 +1000

    model param adjustments

--- src/commands/get_prompt_response.ts
+++ src/commands/get_prompt_response.ts
@@ -19,13 +19,14 @@ export const get_prompt_response = async (prompt: string, char: CharacterProps):
     //     n: 1,
     // });
     const completion = await openai.completions.create({
-        prompt: "This is " + char.name + "'s biography: " + char.bio + "\n" + "you are " + char.name + ", respond to this: \"" + prompt + "\"",
+        prompt: "This is " + char.name + "'s biography: " + char.bio + "\n" + "respond to this: \"" + prompt + "\" as if you were " + char.name,
         model: "text-davinci-002",
         max_tokens: 180,
         temperature: 0.9,
         top_p: 1,
-        presence_penalty: 1,
-        frequency_penalty: 1,
+        presence_penalty: 0.5,
+        frequency_penalty: 0.5,
+        best_of: 1,
         n: 1,
     });
     // if (!completion || !completion.choices[0].message.content || completion.choices[0].message.content.split(' ').length < 2 || completion.choices[0].message.content.length > 1980) {
