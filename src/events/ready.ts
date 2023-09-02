45402a0437a0c76b1e5434639deb23b6377c2ed9
p4stoboy <b1gh@protronmail.com>
2023-09-02 14:03:15 +1000

    fixes to command registration patterns

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,4 @@
-import {Client, Events} from 'discord.js';
+import {ChatInputApplicationCommandData, Client, Events} from 'discord.js';
 const config = require('../config.json');
 import {register_commands} from "../commands/register_commands";
 
@@ -9,6 +9,13 @@ export const ready = {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
     client.user?.setAvatar(config.avatar_url).catch((e) => console.log(e));
     client.user?.setUsername(config.username);
+    client.application?.commands.set([
+      {
+        name: "newchar",
+        description: "Create a new character",
+        options: []
+      }
+    ]);
     client.guilds.cache.forEach(async (guild) => {
       try {
         await register_commands(client, guild.id);
