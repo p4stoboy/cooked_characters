3306da726f4a0fab81769d3d0e1dfe981e3a6057
p4stoboy <b1gh@protronmail.com>
2023-09-02 13:38:54 +1000

    more fixes to db connector routines

--- src/db/get_guild_chars.ts
+++ src/db/get_guild_chars.ts
@@ -2,8 +2,13 @@ import get_db_connection from "./conn";
 import {CharacterProps} from "../types/CharacterProps";
 
 export const get_guild_chars = async (guild_id: string) => {
-    const conn = await get_db_connection();
-    const [rows] = await conn.query(`SELECT * FROM characters WHERE guild_id = ?`, [guild_id]);
-    await conn.end();
-    return rows as CharacterProps[];
+    try {
+        const conn = await get_db_connection();
+        const rows = await conn.query(`SELECT * FROM characters WHERE guild_id = ?`, [guild_id]);
+        await conn.end();
+        return rows as CharacterProps[];
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
 }
