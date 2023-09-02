4de5affe46d5081865cad75525bb5218df555a61
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2023-09-02 17:21:10 +1000

    model change to davinci

--- src/commands/get_prompt_response.ts
+++ src/commands/get_prompt_response.ts
@@ -18,18 +18,20 @@ export const get_prompt_response = async (prompt: string, char: CharacterProps):
     //     frequency_penalty: 1,
     //     n: 1,
     // });
-    const completion: ChatCompletion = await openai.chat.completions.create({
-        messages: messages,
+    const completion = await openai.completions.create({
+        prompt: "This is " + char.name + "'s biography: " + char.bio + "\n" + "you are " + char.name + ", respond to this: \"" + prompt + "\"",
         model: "text-davinci-002",
-        max_tokens: 200,
+        max_tokens: 180,
         temperature: 0.9,
         top_p: 1,
         presence_penalty: 1,
         frequency_penalty: 1,
         n: 1,
     });
-    if (!completion || !completion.choices[0].message.content || completion.choices[0].message.content.split(' ').length < 2 || completion.choices[0].message.content.length > 1980) {
+    // if (!completion || !completion.choices[0].message.content || completion.choices[0].message.content.split(' ').length < 2 || completion.choices[0].message.content.length > 1980) {
+    if (!completion || !completion.choices[0].text || completion.choices[0].text.split(' ').length < 2 || completion.choices[0].text.length > 1980) {
         return "Error";
     }
-    return completion.choices[0].message.content;
+    return completion.choices[0].text;
+    // return completion.choices[0].message.content;
 }
