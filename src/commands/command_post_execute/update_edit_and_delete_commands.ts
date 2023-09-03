5e21293943f3e53c7144d027f64318082100aad5
p4stoboy <b1gh@protronmail.com>
2023-09-03 11:18:29 +1000

    Defensive stuff around guild command creation

--- src/commands/command_post_execute/update_edit_and_delete_commands.ts
+++ src/commands/command_post_execute/update_edit_and_delete_commands.ts
@@ -7,10 +7,14 @@ export const update_edit_and_delete_commands = async (guild_id: string, client: 
     const guild = await client.guilds.fetch(guild_id);
     const new_edit_command = await editchar(guild_id);
     const old_edit_command = guild.commands.cache.reduce((a,b) => b.name === "editchar" ? b : a);
-    await guild.commands.delete(old_edit_command);
+    if (old_edit_command) {
+        await guild.commands.delete(old_edit_command);
+    }
     await guild.commands.create(new_edit_command);
     const new_delete_command = await deletechar(guild_id);
     const old_delete_command = guild.commands.cache.reduce((a,b) => b.name === "deletechar" ? b : a);
-    await guild.commands.delete(old_delete_command);
+    if (old_delete_command) {
+        await guild.commands.delete(old_delete_command);
+    }
     await guild.commands.create(new_delete_command);
 }
