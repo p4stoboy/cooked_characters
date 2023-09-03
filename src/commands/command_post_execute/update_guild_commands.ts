0563440cdf088a91ce008769efb68c47de11c93d
p4stoboy <b1gh@protronmail.com>
2023-09-03 14:26:26 +1000

    Mass simplification of command handling

--- src/commands/command_post_execute/update_guild_commands.ts
+++ src/commands/command_post_execute/update_guild_commands.ts
@@ -6,15 +6,15 @@ import {get_guild_say_command} from "../get_guild_say_command";
 
 export const update_guild_commands = async (guild_id: string, c: Controller, fresh: boolean = false) => {
     const guild = await c.client.guilds.fetch(guild_id);
-    const old_say_command = guild.commands.cache.reduce((a,b) => b.name === "say" ? b : a);
-    const old_edit_command = guild.commands.cache.reduce((a,b) => b.name === "editchar" ? b : a);
-    const old_delete_command = guild.commands.cache.reduce((a,b) => b.name === "deletechar" ? b : a);
 
     const new_say_command = await get_guild_say_command(guild_id);
     const new_edit_command = await get_guild_edit_command(guild_id);
     const new_delete_command = await get_guild_delete_command(guild_id);
 
     if (!fresh) {
+        const old_say_command = guild.commands.cache.reduce((a,b) => b.name === "say" ? b : a);
+        const old_edit_command = guild.commands.cache.reduce((a,b) => b.name === "editchar" ? b : a);
+        const old_delete_command = guild.commands.cache.reduce((a,b) => b.name === "deletechar" ? b : a);
         await guild.commands.edit(old_say_command, new_say_command);
         await guild.commands.edit(old_edit_command, new_edit_command);
         await guild.commands.edit(old_delete_command, new_delete_command);
