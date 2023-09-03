50e56f2fd0333d220123e5ff6553abf818cff626
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2023-09-03 15:42:03 +1000

    moved command functions to global map and parameters per guild (set against gateway, not stored locally).

--- src/commands/command_post_execute/update_guild_commands.ts
+++ src/commands/command_post_execute/update_guild_commands.ts
@@ -8,7 +8,7 @@ import {get_guild_chars} from "../../db/get_guild_chars";
 export const update_guild_commands = async (guild_id: string, c: Controller, fresh: boolean = false) => {
     const guild = await c.client.guilds.fetch(guild_id);
     const chars = await get_guild_chars(guild_id);
-    if (chars.length === 0 || fresh) {
+    if (chars.length === 0) {
         guild.commands.set([]);
         return;
     }
@@ -17,6 +17,7 @@ export const update_guild_commands = async (guild_id: string, c: Controller, fre
     const new_delete_command = await get_guild_delete_command(guild_id);
 
     // if (!fresh) {
+    if (guild.commands.cache.size !== 0) {
         const old_say_command = guild.commands.cache.reduce((a,b) => b.name === "say" ? b : a);
         const old_edit_command = guild.commands.cache.reduce((a,b) => b.name === "editchar" ? b : a);
         const old_delete_command = guild.commands.cache.reduce((a,b) => b.name === "deletechar" ? b : a);
@@ -24,12 +25,12 @@ export const update_guild_commands = async (guild_id: string, c: Controller, fre
         await guild.commands.edit(old_edit_command, new_edit_command);
         await guild.commands.edit(old_delete_command, new_delete_command);
         console.log(`Successfully EDITED application commands in ${guild.name}.`)
-    // } else {
-    //     await guild.commands.create(new_say_command);
-    //     await guild.commands.create(new_edit_command);
-    //     await guild.commands.create(new_delete_command);
-    //     console.log(`Successfully CREATED application commands in ${guild.name}.`)
-    // }
+    } else {
+        await guild.commands.create(new_say_command);
+        await guild.commands.create(new_edit_command);
+        await guild.commands.create(new_delete_command);
+        console.log(`Successfully CREATED application commands in ${guild.name}.`)
+    }
 
     // c.commands.get(guild_id)?.set(new_say_command.name, new_say_command);
     // c.commands.get(guild_id)?.set(new_edit_command.name, new_edit_command);
