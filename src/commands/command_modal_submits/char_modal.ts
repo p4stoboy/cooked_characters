69e0e59485a83d2ca4f75a2408bfe214e6a1f5b8
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2023-09-03 16:11:01 +1000

    setValue instead of placeHolder

--- src/commands/command_modal_submits/char_modal.ts
+++ src/commands/command_modal_submits/char_modal.ts
@@ -4,20 +4,20 @@ import {ActionRowBuilder, ModalBuilder, TextInputBuilder} from "discord.js";
 export const get_char_modal = (char?: CharacterProps) => {
 
     const default_name = char ? char.name : "";
-    const default_bio = char ? "current bio has been dmed": "";
+    const default_bio = char ? char.bio : "";
     const default_image_url = char ? char.image_url ? char.image_url : "" : "";
     const default_color = char ? char.color ? char.color : "#FF6600" : "#FF6600";
     const id = char ? char.id : "";
 // create discord modal with fields for user to create new character
     const char_modal = new ModalBuilder().setTitle('New Character').setCustomId((char ? `editchar_${char.id}` : "new_char"));
 
-    const name = new TextInputBuilder().setLabel("Character's name").setCustomId("name").setPlaceholder(default_name).setRequired(true).setStyle(1).setMaxLength(32);
+    const name = new TextInputBuilder().setLabel("Character's name").setCustomId("name").setValue(default_name).setRequired(true).setStyle(1).setMaxLength(32);
 
-    const bio = new TextInputBuilder().setLabel("Character's bio").setCustomId("bio").setPlaceholder(default_bio).setRequired(true).setMinLength(50).setMaxLength(2000).setStyle(2);
+    const bio = new TextInputBuilder().setLabel("Character's bio").setCustomId("bio").setValue(default_bio).setRequired(true).setMinLength(50).setMaxLength(2000).setStyle(2);
 
-    const image_url= new TextInputBuilder().setLabel("Avatar URL (optional)").setCustomId("image_url").setPlaceholder(default_image_url).setRequired(false).setStyle(1);
+    const image_url= new TextInputBuilder().setLabel("Avatar URL (optional)").setCustomId("image_url").setValue(default_image_url).setRequired(false).setStyle(1);
 
-    const color = new TextInputBuilder().setLabel("Hex color for embed").setCustomId("color").setPlaceholder(default_color).setRequired(false).setStyle(1).setMinLength(7).setMaxLength(7).setPlaceholder("#FF6600");
+    const color = new TextInputBuilder().setLabel("Hex color for embed").setCustomId("color").setValue(default_color).setRequired(false).setStyle(1).setMinLength(7).setMaxLength(7).setPlaceholder("#FF6600");
 
     const row1 = new ActionRowBuilder<TextInputBuilder>().addComponents(name);
     const row2 = new ActionRowBuilder<TextInputBuilder>().addComponents(bio);
