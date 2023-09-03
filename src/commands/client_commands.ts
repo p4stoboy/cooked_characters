9df2aa435c116ef4d8cdbd5129eb0878aa62714c
p4stoboy <43506721+p4stoboy@users.noreply.github.com>
2023-09-03 15:52:53 +1000

    bio command / fixed db update param

--- src/commands/client_commands.ts
+++ src/commands/client_commands.ts
@@ -2,11 +2,13 @@ import {ClientCommand} from "../types/ClientCommand";
 import {say_execute} from "./get_guild_say_command";
 import {edit_execute} from "./get_guild_edit_command";
 import {delete_execute} from "./get_guild_delete_command";
+import {bio_execute} from "./get_guild_bio_command";
 
 
 const client_commands = new Map<string, ClientCommand>();
 client_commands.set(`say`, say_execute);
 client_commands.set(`editchar`, edit_execute);
 client_commands.set(`deletechar`, delete_execute);
+client_commands.set(`bio`, bio_execute);
 
 export default client_commands;
--- src/commands/command_post_execute/update_guild_commands.ts
+++ src/commands/command_post_execute/update_guild_commands.ts
@@ -3,6 +3,7 @@ import {get_guild_delete_command} from "../get_guild_delete_command";
 import {Controller} from "../../types/Controller";
 import {get_guild_say_command} from "../get_guild_say_command";
 import {get_guild_chars} from "../../db/get_guild_chars";
+import {get_guild_bio_command} from "../get_guild_bio_command";
 
 
 export const update_guild_commands = async (guild_id: string, c: Controller, fresh: boolean = false) => {
@@ -15,24 +16,23 @@ export const update_guild_commands = async (guild_id: string, c: Controller, fre
     const new_say_command = await get_guild_say_command(guild_id);
     const new_edit_command = await get_guild_edit_command(guild_id);
     const new_delete_command = await get_guild_delete_command(guild_id);
+    const new_bio_command = await get_guild_bio_command(guild_id);
 
-    // if (!fresh) {
     if (guild.commands.cache.size !== 0) {
         const old_say_command = guild.commands.cache.reduce((a,b) => b.name === "say" ? b : a);
         const old_edit_command = guild.commands.cache.reduce((a,b) => b.name === "editchar" ? b : a);
         const old_delete_command = guild.commands.cache.reduce((a,b) => b.name === "deletechar" ? b : a);
+        const old_bio_command = guild.commands.cache.reduce((a,b) => b.name === "bio" ? b : a);
         await guild.commands.edit(old_say_command, new_say_command);
         await guild.commands.edit(old_edit_command, new_edit_command);
         await guild.commands.edit(old_delete_command, new_delete_command);
+        await guild.commands.edit(old_bio_command, new_bio_command);
         console.log(`Successfully EDITED application commands in ${guild.name}.`)
     } else {
         await guild.commands.create(new_say_command);
         await guild.commands.create(new_edit_command);
         await guild.commands.create(new_delete_command);
+        await guild.commands.create(new_bio_command);
         console.log(`Successfully CREATED application commands in ${guild.name}.`)
     }
-
-    // c.commands.get(guild_id)?.set(new_say_command.name, new_say_command);
-    // c.commands.get(guild_id)?.set(new_edit_command.name, new_edit_command);
-    // c.commands.get(guild_id)?.set(new_delete_command.name, new_delete_command);
 }
